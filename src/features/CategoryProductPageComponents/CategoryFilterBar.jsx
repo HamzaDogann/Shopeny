465b013d22fc6b1b6333b7728ead1a167549fbb6
HamzaDogann <hamzadgn011@gmail.com>
2024-07-19 23:41:27 +0300

    Category Products Page Component Implemented

--- src/features/CategoryProductPageComponents/CategoryFilterBar.jsx
+++ src/features/CategoryProductPageComponents/CategoryFilterBar.jsx
@@ -1,20 +1,164 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Checkbox from '../../shared/helpers/Checkbox';
+import { Slider, Box, Rating } from '@mui/material';
+import { MdClose } from "react-icons/md";
 
-function CategoryFilterBar() {
-  const brands = ['Apple', 'Samsung', 'Sony', 'LG', 'Huawei', 'Xiaomi'];
+function CategoryFilterBar({closeFilterMenuFunc}) {
+
+  //Filter Options
+  const brands = ['Apple', 'Samsung', 'Sony', 'LG', 'Huawei', 'Xiaomi', 'Oppo'];
+  const colors = [
+    { name: 'Siyah', code: '#1a1a1a' },
+    { name: 'Beyaz', code: '#ffffff' },
+    { name: 'Gri', code: '#60676b' },
+    { name: 'Kırmızı', code: '#d93f3f' },
+    { name: 'Yeşil', code: '#19d47a' },
+    { name: 'Mavi', code: '#3c6ade' },
+    { name: 'Turuncu', code: '#f27d59' }
+  ];
+
+  //Filter States
+  const [checkedBrands, setCheckedBrands] = useState([]);
+  const [priceRange, setPriceRange] = useState([20, 180]);
+  const [checkedColors, setCheckedColors] = useState([]);
+  const [rating, setRating] = useState(0);
+  //! ======= HANDLE FILTER METHODS =======
+
+  // Handle Brands
+  const handleCheckboxChange = (brand) => {
+    setCheckedBrands(prevState =>
+      prevState.includes(brand)
+        ? prevState.filter(item => item !== brand)
+        : [...prevState, brand]
+    );
+  };
+
+  //Handle Price
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setPriceRange((prevPriceRange) => {
+      return name === 'min'
+        ? [Math.min(value, prevPriceRange[1]), prevPriceRange[1]]
+        : [prevPriceRange[0], Math.max(value, prevPriceRange[0])];
+    });
+  };
+
+  const handleSliderChange = (event, newValue) => {
+    setPriceRange(newValue);
+  };
+
+  //Handle Color
+  const handleColorCheckboxChange = (color) => {
+    setCheckedColors(prevState =>
+      prevState.includes(color)
+        ? prevState.filter(item => item !== color)
+        : [...prevState, color]
+    );
+  };
+
+
+  //Handle Rating
+  const handleRatingChange = (event, newValue) => {
+    setRating(newValue);
+  };
 
   return (
     <div className="filter-box">
-      <div className="brands-box">
-        <p>Markalar</p>
+      <div className="filter-options-box">
+        {/* Brands */}
+        <p className='brands-title'>Markalar</p>
         <ul>
           {brands.map((brand, index) => (
             <li key={index}>
-              <Checkbox id={`brand-${index}`} label={brand} />
+              <Checkbox
+                id={`brand-${index}`}
+                label={brand}
+                checked={checkedBrands.includes(brand)}
+                onChange={() => handleCheckboxChange(brand)}
+              />
+              <label htmlFor={`brand-${index}`}>{brand}</label>
             </li>
           ))}
         </ul>
+
+        {/* Prices */}
+        <p className='price-title'>Fiyat Aralığı</p>
+        <Box className="price-range-slider" sx={{ width: 300, padding: 3 }}>
+          <Box display="flex" justifyContent="space-between" mb={2}>
+            <div className='custom-input-box'>
+              <input
+                className='custom-input'
+                type="number"
+                name="min"
+                value={priceRange[0]}
+                onChange={handleInputChange}
+                min={0}
+                max={priceRange[1]}
+              />
+              <p>₺</p>
+            </div>
+
+            <span className='price-dividing-line' >-</span>
+
+            <div className='custom-input-box'>
+              <input
+                className='custom-input'
+                type="number"
+                name="max"
+                value={priceRange[1]}
+                onChange={handleInputChange}
+                min={priceRange[0]}
+                max={1000}
+              />
+              <p>₺</p>
+            </div>
+          </Box>
+          <Slider
+            value={priceRange}
+            onChange={handleSliderChange}
+            valueLabelDisplay="auto"
+            min={0}
+            max={1000}
+            step={10}
+          />
+        </Box>
+
+        {/* Colors */}
+        <p style={{ marginTop: "5px" }} className='colors-title'>Renkler</p>
+        <ul>
+          {colors.map((color, index) => (
+            <li key={index}>
+              <Checkbox
+                id={`color-${index}`}
+                label={color.name}
+                checked={checkedColors.includes(color.name)}
+                onChange={() => handleColorCheckboxChange(color.name)}
+              />
+              <label className='color-box' htmlFor={`color-${index}`}>
+                <span
+                  className="color-circle"
+                  style={{ backgroundColor: color.code }}
+                />
+                <span>{color.name}</span>
+              </label>
+            </li>
+          ))}
+        </ul>
+
+        {/* Customer Rating */}
+        <p className='rating-title'>Müşteri Puanı</p>
+        <div>
+          <Rating
+            name="half-rating"
+            value={rating}
+            precision={0.5}
+            onChange={handleRatingChange}
+          />
+        </div>
+
+        {/* Filter Button */}
+        <button className='filter-btn'>Filtreyi Uygula</button>
+        <button onClick={closeFilterMenuFunc} className='close-filter-menu-btn'><MdClose /></button>
       </div>
     </div>
   );
--- src/pages/CategoryProductsPage/CategoryProducts.jsx
+++ src/pages/CategoryProductsPage/CategoryProducts.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navigate, useParams } from 'react-router-dom';
 import { validCategories } from "../../constants/categories"
+import { MdFilterListAlt } from "react-icons/md";
 
 import RadioButton from "../../shared/helpers/RadioButton"
 import "./CategoryProducts.scss"
@@ -10,37 +11,69 @@ function CategoryProducts() {
 
   const { categoryName } = useParams();
 
+  if (!validCategories.includes(categoryName)) {
+    return <Navigate to="/" />;
+  }
+
   const [sortOption, setSortOption] = useState('priceAsc');
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1100);
 
   const handleSortChange = (event) => {
     setSortOption(event.target.value);
   };
 
-  if (!validCategories.includes(categoryName)) {
-    return <Navigate to="/" />;
-  }
+  const showFilterMenu = () => {
+    setIsFilterVisible(true);
+  };
+
+  const hideFilterMenu = () => {
+    setIsFilterVisible(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1100);
+      if (window.innerWidth > 1100) {
+        setIsFilterVisible(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
 
   return (
     <div className='category-products-box'>
 
       <div className='top-filter-options-box'>
-        <div className='is-stock-box'>
-          <RadioButton />
-          <span>Stoktakiler</span>
+        <div className='stock-and-sorting-box'>
+          <div className='is-stock-box'>
+            <RadioButton />
+            <span>Stoktakiler</span>
+          </div>
+
+          <select name="sorting" id="sorting" value={sortOption} onChange={handleSortChange}>
+            <option value="priceAsc">Fiyata Göre Artan</option>
+            <option value="priceDesc">Fiyata Göre Azalan</option>
+            <option value="oldToNew">Eskiden Yeniye</option>
+            <option value="newToOld">Yeniden Eskiye</option>
+          </select>
         </div>
 
-        <select name="sorting" id="sorting" value={sortOption} onChange={handleSortChange}>
-          <option value="priceAsc">Fiyata Göre Artan</option>
-          <option value="priceDesc">Fiyata Göre Azalan</option>
-          <option value="oldToNew">Eskiden Yeniye</option>
-          <option value="newToOld">Yeniden Eskiye</option>
-        </select>
+        {isMobile && (
+          <button className='mobile-filter-btn' onClick={showFilterMenu}>
+            <MdFilterListAlt className='filter-icon' />
+            <span>Filtreleme Seçenekleri</span>
+          </button>
+        )}
       </div>
 
       <div className='category-products'>
-        <div className='category-filter-bar'>
+        <div className='category-filter-bar' style={{ display: isMobile && isFilterVisible ? 'flex' : !isMobile ? 'flex' : 'none' }}>
           {/* Bazı propslar alıcaklar */}
-          <CategoryFilterBar />
+          <CategoryFilterBar closeFilterMenuFunc={hideFilterMenu} />
         </div>
         <div className='category-products-bar'>
           {/* Bazı propslar alıcaklar */}
