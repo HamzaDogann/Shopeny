0afcc9ccd6612139b187cb9c1cb31160447ea2cc
HamzaDogann <hamzadgn011@gmail.com>
2024-07-27 19:28:48 +0300

    Comments lines removed for Layout.jsx

--- src/shared/layout/Layout.jsx
+++ src/shared/layout/Layout.jsx
@@ -4,10 +4,8 @@ import { useLocation } from 'react-router-dom';
 const Layout = ({ children }) => {
   const location = useLocation();
 
-  // Aktif URL'nin 'giris-yap', 'uye-ol' veya 'sifre-yenileme' olup olmadığını kontrol et
   const isAuthPage = ['/giris-yap', '/uye-ol', '/sifre-yenileme'].includes(location.pathname);
 
-  // Eğer bu sayfalardan biriyse null döndür
   if (isAuthPage) return null;
 
   return <div>{children}</div>;
